56d741e40f8a43dc65f0abfe7950f272f2cfa903
Abhiraj Anurag Sinha <96344025+AbhirajSinha179@users.noreply.github.com>
2023-09-25 18:25:58 +0530

    Update main.cpp
    added user input of range of numbers

--- main.cpp
+++ main.cpp
@@ -7,17 +7,30 @@ using namespace std;
 
 int main() {
     srand(time(0));
-    const int minNumber = 1;
-    const int maxNumber = 100;
 
     char playAgain;
     
     do {
-        int secretNumber = rand() % (maxNumber - minNumber + 1) + minNumber;
+        // initialising range at the start of each game
+        int minNumber = 1; 
+        int maxNumber = 100;
+        
         int attempts = 0;
         int guess;
         
         cout << "Welcome to the Number Guessing Game!" << endl;
+        // taking input of range from user
+        cout<<"Enter the range in which you wish to guess: (low, high)"<<endl;
+        cin>>minNumber>>maxNumber;
+        // Invalid input
+        while(minNumber>=maxNumber-1){
+            cout<<"Invalid input, try again!"<<endl;
+            cout<<"Enter the range in which you wish to guess: (low, high)"<<endl;
+            cin>>minNumber>>maxNumber;
+        }
+        // number to be guessed
+        int secretNumber = rand() % (maxNumber - minNumber + 1) + minNumber;
+        
         cout << "I'm thinking of a number between " << minNumber << " and " << maxNumber << "." << endl;
         
         while (true) {
